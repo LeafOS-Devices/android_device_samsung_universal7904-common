77e4078c9a0602e06b884b47d6cc63a053922c0b
SamarV-121 <samarvispute121@pm.me>
2023-01-25 16:00:10 +0530

    universal7904: vibrator: Fix amplitude control
     * Our sysfs node expects value between 1000-10000 while setAmplitude() reports value between 0.0f-1.0f
       multiply received amplitude by max intensity
    
    Change-Id: Iedcdbb455b67b9466a76e53ca9273cbcdcede059

--- aidl/vibrator/Vibrator.cpp
+++ aidl/vibrator/Vibrator.cpp
@@ -157,20 +157,13 @@ ndk::ScopedAStatus Vibrator::getSupportedEffects(std::vector<Effect>* _aidl_retu
 ndk::ScopedAStatus Vibrator::setAmplitude(float amplitude) {
     uint32_t intensity;
 
-    if (amplitude == 0) {
-        return ndk::ScopedAStatus::fromExceptionCode(EX_ILLEGAL_ARGUMENT);
+    if (amplitude <= 0.0f || amplitude > 1.0f) {
+        return ndk::ScopedAStatus(AStatus_fromExceptionCode(EX_ILLEGAL_ARGUMENT));
     }
 
-    LOG(DEBUG) << "Setting amplitude: " << (uint32_t)amplitude;
+    LOG(DEBUG) << "Setting amplitude: " << amplitude;
 
-    intensity = std::lround((amplitude - 1) * INTENSITY_MAX / 254.0);
-    if (intensity > INTENSITY_MAX) {
-        intensity = INTENSITY_MAX;
-    }
-
-    if (intensity == 0) {
-        return ndk::ScopedAStatus::fromExceptionCode(EX_ILLEGAL_ARGUMENT);
-    }
+    intensity = amplitude * INTENSITY_MAX;
 
     LOG(DEBUG) << "Setting intensity: " << intensity;
 
@@ -275,11 +268,11 @@ uint8_t Vibrator::strengthToAmplitude(EffectStrength strength, ndk::ScopedAStatu
 
     switch (strength) {
         case EffectStrength::LIGHT:
-            return 64;
+            return INTENSITY_LIGHT;
         case EffectStrength::MEDIUM:
-            return 128;
+            return INTENSITY_MEDIUM;
         case EffectStrength::STRONG:
-            return 255;
+            return INTENSITY_STRONG;
     }
 
     *status = ndk::ScopedAStatus::fromExceptionCode(EX_UNSUPPORTED_OPERATION);
